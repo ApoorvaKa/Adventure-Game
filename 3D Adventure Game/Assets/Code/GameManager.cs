c78cd0547095cdd69d32b8912d7b428bda26ed8f
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2021-11-10 16:03:04 -0500

    Pause Menu Added
    Pause menu added. UI elements and code in game manager

--- 3D Adventure Game/Assets/Code/GameManager.cs
+++ 3D Adventure Game/Assets/Code/GameManager.cs
@@ -1,17 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    public GameObject quitButton;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        Resume();
         
+        #if UNITY_WEBGL
+        quitButton.SetActive(false);
+        #endif
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (PublicVars.paused){
+                Resume();
+
+            }
+            else{
+                pauseMenu.SetActive(true);
+                PublicVars.paused = true;
+                Time.timeScale = 0;
+            }
+        }
     }
+
+    public void Resume(){
+        pauseMenu.SetActive(false);
+        PublicVars.paused = false;
+        Time.timeScale = 1;
+    }
+
+    public void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Quit(){
+        Application.Quit();
+        print("Quit called");
+    }
+
 }
--- 3D Adventure Game/Assets/Code/PublicVars.cs
+++ 3D Adventure Game/Assets/Code/PublicVars.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class PublicVars : MonoBehaviour
 {
+    // rooms and keys
     public static string currentRoom = "";
     public static int numKeys = 0;
     public static bool greenKey = false;
     public static bool redKey = false;
     public static bool blueKey = false;
     
-    //health system
+    // health system
     public static int life = 1;
     public static int numHearts = 3;
+
+    // pause menu
+    public static bool paused = false;
 }
